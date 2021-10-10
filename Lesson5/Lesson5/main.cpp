c641f8e225ccb7c9296c89afe6ab8bb8f2b04665
maxim <maxim_sdrv@mail.ru>
2021-10-10 20:59:33 +0300

    vs19

--- Lesson5/Lesson5/main.cpp
+++ Lesson5/Lesson5/main.cpp
@@ -7,6 +7,8 @@
 #include <list>
 #include <filesystem>
 
+
+
 std::string readFile(const std::filesystem::path& file) {
     std::ifstream f(file, std::ios::binary);
     const auto sz = std::filesystem::file_size(file);
@@ -23,15 +25,15 @@ std::string readFile(const std::filesystem::path& file) {
 class Sentences {
 public:
     explicit Sentences(const std::string& line) {
-        int id {};
-        std::string temp {};
+        int id{};
+        std::string temp{};
         for (char c : line) {
             if (c != '.') {
                 temp += c;
                 ++id;
             }
             else {
-                m_Sentences.insert({id, temp});
+                m_Sentences.insert({ id, temp });
                 temp.clear();
                 id = 0;
             }
@@ -50,21 +52,21 @@ private:
 
 
 template<typename T>
-void uniqueWords(T &begin, T &end) {
-//Manual, without STL.
-    /*for (auto ptr = begin; ptr != end; ++ptr) {
-        T str;
-        for (str = begin; str != ptr; ++str) {
-            if (*ptr == *str) {
-                break;
+void uniqueWords(T& begin, T& end) {
+    //Manual, without STL.
+        /*for (auto ptr = begin; ptr != end; ++ptr) {
+            T str;
+            for (str = begin; str != ptr; ++str) {
+                if (*ptr == *str) {
+                    break;
+                }
             }
-        }
-        if (ptr == str) {
-            std::cout << *ptr << " ";
-        }
+            if (ptr == str) {
+                std::cout << *ptr << " ";
+            }
 
-    }*/
-//Used STL.
+        }*/
+        //Used STL.
     std::map<std::string, int> words;
     for (auto ptr = begin; ptr != end; ++ptr) {
         if (words.count(*ptr) == 0) {
@@ -78,17 +80,17 @@ void uniqueWords(T &begin, T &end) {
 }
 
 int main() {
-//1.
-    std::vector<std::string> s{"Hello", "world", "i", "i", "so", "tired", "so", "learn", "Cpp", "you", "know", "you"};
+    //1.
+    std::vector<std::string> s{ "Hello", "world", "i", "i", "so", "tired", "so", "learn", "Cpp", "you", "know", "you" };
     auto begin = s.begin();
     auto end = s.end();
-    for (const auto &i: s) {
+    for (const auto& i : s) {
         std::cout << i << " ";
     }
     std::cout << std::endl;
     uniqueWords(begin, end);
     std::cout << std::endl;
-    std::list<std::string> s2 {"Hello", "world", "i", "i", "so", "tired", "so", "learn", "Cpp", "you", "know", "you"};
+    std::list<std::string> s2{ "Hello", "world", "i", "i", "so", "tired", "so", "learn", "Cpp", "you", "know", "you" };
     auto list_begin = s2.begin();
     auto list_end = s2.end();
     std::cout << "List iterators: " << std::endl;
@@ -99,39 +101,10 @@ int main() {
     uniqueWords(list_begin, list_end);
     std::cout << std::endl;
 
-//2.
+    //2.
     std::string fileName = readFile("../sentences.txt");
     Sentences file(fileName);
     std::cout << file;
 
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
