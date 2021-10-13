98d057c0cd0d2481439b839263a2e73418951d8c
maxim <maxim_sdrv@mail.ru>
2021-10-13 16:59:00 +0300

    completed homework of lesson6

--- Lesson6/Lesson6/Source.cpp
+++ Lesson6/Lesson6/Source.cpp
@@ -2,12 +2,9 @@
 #include <thread>
 #include <mutex>
 #include <vector>
-#include <algorithm>
 #include <numeric>
-#include <random>
 #include <future>
 #include <map>
-#include <iterator>
 
 using namespace std::chrono_literals;
 using Map = std::map<int, std::string>;
@@ -17,40 +14,45 @@ std::mutex mtx;
 
 class Host {
 public:
-	
-	
-	Map addThing(int cost, std::string name) {
-		std::lock_guard lg(mtx);
-		std::cout << "Thread start" << std::endl;
-		std::cout << std::this_thread::get_id() << std::endl;
+	void addThing(int cost, std::string name) {
+        std::lock_guard lg(m);
+        std::this_thread::sleep_for(1000ms);
+		std::cout << "ID thread = " << std::this_thread::get_id() <<
+        " ===================\tADD THING STARTED\t ===================" << std::endl;
+        std::this_thread::sleep_for(1000ms);
+
+        std::cout << "HOST ADDED " << name << " " << cost << "$" << std::endl;
+
 		m_Vault.insert({ cost, std::move(name) });
 
-		return m_Vault;
+        std::cout << "ID thread = " << std::this_thread::get_id() <<
+                  " ===================\tADD THING STOPPED\t ===================" << std::endl;
 	}
-	bool stealThing() {
-		
+	void stealThing() {
+        std::this_thread::sleep_for(1000ms);
+        std::lock_guard lg(m);
+        std::cout << "ID thread = " << std::this_thread::get_id() <<
+                  " ===================\tSTEAL THING STARTED\t ===================" << std::endl;
+        std::this_thread::sleep_for(1000ms);
+
 		auto exp = std::max_element(m_Vault.begin(), m_Vault.end(), [](const auto& lhs, const auto& rhs) {
 			return lhs.first < rhs.first;
 		});
-		std::cout << std::this_thread::get_id() << std::endl;
-		std::lock_guard lg2(mtx);
-		if (m_Vault.empty()) {
-			std::cout << "map is empty" << std::endl;
 
-			return false;
+		if (m_Vault.empty()) {
+			std::cout << "HOUSE IS EMPTY: " << std::endl;
 		}
 		else {
-			std::cout << "Thread 2" << std::endl;
-			std::for_each(m_Vault.begin(), m_Vault.end(), [](const auto& p) {
-				std::cout << p.first << " " << p.second << std::endl;
-			});
-			m_Vault.erase(exp);
-			std::cout << "Stop" << std::endl;
+            std::this_thread::sleep_for(500ms);
+            std::cout << "THIEF STOLE : " << exp->second << std::endl;
 
-			return true;
+			m_Vault.erase(exp);
 		}
+        std::cout << "ID thread = " << std::this_thread::get_id() <<
+        " ===================\tSTEAL THING STOPPED\t ===================" << std::endl;
 		
 	}
+
 	friend std::ostream& operator<<(std::ostream& os, const Host& host) {
 		for (const auto& i : host.m_Vault) {
 			os << i.first << " " << i.second << std::endl;
@@ -59,18 +61,19 @@ public:
 	}
 	
 private:
-	 Map m_Vault;
+    Map m_Vault;
+    std::mutex m;
 };
 
 int SumToVectors(const std::vector<int>& one, const std::vector<int>& two) {
 	std::future<int> f = std::async([&one] {
-		std::cout << "Thread N2 = ";
+        std::cout << "Thread N2 =";
 		return std::accumulate(one.begin(), one.end(), 0);
 	});
-	std::cout << "Thread N1" << std::endl;
+	std::cout << "Thread N1 + ";
 	int result = std::accumulate(two.begin(), two.end(), 0);
-	
-	return result + f.get();
+
+    return result + f.get();
 }
 
 
@@ -83,62 +86,58 @@ void pcout(int number) {
 }
 
 
-Map addThing(int cost, std::string name) {
-	Map temp;
-	temp.insert({ cost, std::move(name) });
-	return temp;
-}
 
 int main() {
 //1.
-/*Создайте потокобезопасную оболочку для объекта cout.Назовите ее pcout.Необходимо, чтобы несколько 
+/*Создайте потокобезопасную оболочку для объекта cout.Назовите ее pcout.Необходимо, чтобы несколько
 потоков могли обращаться к pcout и информация выводилась в консоль.Продемонстрируйте работу pcout.*/
-
-	/*std::thread th1(pcout, 1);
+    std::cout << "==============================TASK N1======================================" << std::endl;
+	std::thread th1(pcout, 1);
 	std::thread th2(pcout, 2);
 	std::thread th3(pcout, 3);
 
 	th1.join();
 	th2.join();
-	th3.join();*/
+	th3.join();
 
 //2.
 /*Реализовать функцию, возвращающую i-ое простое число (например, миллионное простое число равно 15485863).
 Вычисления реализовать во вторичном потоке. В консоли отображать прогресс вычисления. 
 */
-
-	/*std::cout << SumToVectors({ 1, 1, 1, 1 }, { 3, 3, 3, 3 });
-	*/
+    std::cout << "==============================TASK N2======================================" << std::endl;
+    std::cout << "Result = ";
+	std::cout << SumToVectors({ 1, 1, 1, 1 }, { 3, 3, 3, 3 }) << std::endl;
 
 /*Промоделировать следующую ситуацию.Есть два человека(2 потока) : хозяин и вор.Хозяин приносит домой вещи.
 При этом у каждой вещи есть своя ценность.Вор забирает вещи, каждый раз забирает вещь с наибольшей ценностью.
 */
+    std::cout << "==============================TASK N3======================================" << std::endl;
 	Host general;
-	/*Host host_class;*/
 
-	auto host1(std::async(std::launch::async, &Host::addThing, general, 1000, "notebook"));
-	auto host2 (std::async(std::launch::async, &Host::addThing, general, 500, "TV"));
-	auto host3(std::async(std::launch::async, &Host::addThing, general, 100, "phone"));
-	for (const auto& [key, value] : host3.get()) {
-		std::cout << key << " " << value << std::endl;
-	}
-	std::cout << general;
-	/*auto host = std::async(std::launch::async, addThing, 1000, "notebook");
-	for (const auto& [key, value] : host.get()) {
-		std::cout << key << " " << value << std::endl;
-	}*/
-	
-	
-	
-	
+    std::thread host([&general]() {
+        general.addThing(1000, "Notebook");
+        general.addThing(5000, "Car");
+        general.addThing(800, "Bicycle");
+        general.addThing(300, "Mobile Phone");
+    });
+
+    std::thread thief([&general] {
+        general.stealThing();
+    });
+
+    for (size_t i = 0; i <= 10; ++i) {
+        std::cout << "ID of thread = " << std::this_thread::get_id() << "\tmain thread\t" << i << std::endl;
+        std::this_thread::sleep_for(500ms);
+    }
+
+    host.join();
+    thief.join();
+
+
+
 
 	
-	/*host_class.addThing(1000, "notebook");
-	host_class.addThing(200, "phone");
-	host_class.addThing(500, "TV");
-	std::cout << host_class << std::endl;
-	host_class.stealThing();
-	std::cout << host_class << std::endl;*/
+
 
 	return 0;
 }
